f606dbe4fcfa67e2ebdb6cb5ae92d9318e5e9464
Juntan-dev <Juntan@dtbcreation.com>
2026-04-14 16:12:12 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,65 @@
+from datetime import datetime, time
+from zoneinfo import ZoneInfo
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+import os
+BOT_TOKEN = os.environ.get("8264310379:AAHsc8KWiRXYias-znZ0wW050Ecy7gqja1E")
+GROUP_CHAT_ID = -4925926064
+TIMEZONE = ZoneInfo("Asia/Kuala_Lumpur")
+
+BIRTHDAYS = {
+    "Lauren": "07-03",
+    "Edison": "15-04",
+    "Kevin": "23-04",
+    "Tracy": "30-04",
+    "Alfred": "29-05",
+    "Kang Lin": "19-06",
+    "Justin": "23-07",
+    "Alice": "04-08",
+    "Kayden": "05-08",
+    "Khloe": "10-09",
+    "Andrew": "11-09",
+    "Jun Jie": "14-09",
+    "Wen Kang": "27-09",
+    "Komi": "28-11"
+}
+
+async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    await update.message.reply_text(f"Chat ID is: {chat_id}")
+    print("CHAT ID:", chat_id)
+
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is working ✅")
+
+async def check_birthdays(context: ContextTypes.DEFAULT_TYPE):
+    today = datetime.now(TIMEZONE).strftime("%d-%m")
+
+    birthday_people = [
+        name for name, date in BIRTHDAYS.items() if date == today
+    ]
+
+    if birthday_people:
+        names = ", ".join(birthday_people)
+        await context.bot.send_message(
+            chat_id=GROUP_CHAT_ID,
+            text=f"🎉 Today is {names}'s birthday! Wish them!"
+        )
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("id", get_id))
+    app.add_handler(CommandHandler("test", test))
+
+    app.job_queue.run_once(check_birthdays, when=5)
+    app.job_queue.run_daily(
+        check_birthdays,
+        time=time(hour=9, minute=0, tzinfo=TIMEZONE)
+    )
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
